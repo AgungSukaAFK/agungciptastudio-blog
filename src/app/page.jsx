4f003c15208cf542ac5126ada7fa2d96598b0e0c
AgungSukaAFK <wismaagung23@gmail.com>
2024-05-21 21:50:47 +0700

    Add: homepage design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,40 @@
+import { Lato } from "next/font/google";
+
+const lato = Lato({ subsets: ["latin"], weight: ["400", "700"] });
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="home-container">
+      {/* Section pertama */}
+      <section className="home-first">
+        <div className="title">
+          <h1 className={lato.className}>
+            <span>AgungCipta</span> Blog Site
+          </h1>
+        </div>
+
+        <div className="search-container">
+          <input
+            type="search"
+            name="search"
+            id="searchInput"
+            className="search-input"
+            placeholder="Ketikkan pencarian..."
+          />
+          <button className="search-button">Cari</button>
+        </div>
+
+        <div className="description-container">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Ab sint soluta
+            inventore beatae, nesciunt,
+          </p>
+        </div>
+      </section>
+
+      {/* Section kedua */}
+      <section className="home-second"></section>
     </div>
   );
 }
--- src/ui/navbar.jsx
+++ src/ui/navbar.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function navbar() {
   return (
-    <div>
+    <div className="navbar-container">
       <nav className="navbar">
         <div className="logo">
           <Image
