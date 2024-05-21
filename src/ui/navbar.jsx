62787e7acf12a8ac5a405736b94e4fcfedaa424e
AgungSukaAFK <wismaagung23@gmail.com>
2024-05-21 20:31:04 +0700

    Initialize repository

---
+++ src/ui/navbar.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function navbar() {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="logo">
+          <Image
+            src="/web-light-AgungCipta.png"
+            alt="Agungcipta logo"
+            width={60}
+            height={60}
+            className="image-logo"
+          />
+        </div>
+        <div className="menu">X</div>
+      </nav>
+    </div>
+  );
+}
